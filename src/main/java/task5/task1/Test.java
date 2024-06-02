82a3ce37ead6b905b9dcd9c668925fd25f913ec9
Donistr <deniselag@mail.ru>
2024-06-02 13:59:18 +0400

    исправления

--- src/main/java/task5/task1/Test.java
+++ src/main/java/task5/task1/Test.java
@@ -6,14 +6,17 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<>();
         list.add(2);
         list.add(10);
         list.add(3);
         list.add(6);
         list.add(1);
+        list.add(1);
+        list.add(1);
         list.add(4);
         list.add(8);
+        list.add(1);
         list.add(5);
         list.add(7);
         list.add(9);
--- src/main/java/task5/task2/Departments.java
+++ src/main/java/task5/task2/Departments.java
@@ -1,6 +1,7 @@
 package task5.task2;
 
 public enum Departments {
+
     IT,
     HR,
     MANAGEMENT
